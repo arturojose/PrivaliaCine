f0509b6e9899e489ecf71011a804220e316f0eed
arthur <arthureloy@gmail.com>
2018-07-15 10:29:51 +0200

    Commit para carga inicial

---
+++ app/src/main/java/com/privaliacine/SplashScreenActivity.java
@@ -0,0 +1,18 @@
+package com.privaliacine;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_splash_screen);
+
+        Intent intent = new Intent(this, SplashScreenActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
